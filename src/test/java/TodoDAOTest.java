af5e1cdc7437fbb2cbf65e50a7d25c567e626d38
KHG <koo4934@gmail.com>
2025-10-23 16:52:15 +0900

    2025. 10. 23 WebMVC

--- src/test/java/TodoDAOTest.java
+++ src/test/java/TodoDAOTest.java
@@ -1,5 +1,6 @@
 import com.ssg.webmvc.todo.dao.TodoDAO;
 import com.ssg.webmvc.todo.domain.TodoVO;
+import com.sun.tools.javac.comp.Todo;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
@@ -28,7 +29,7 @@ public class TodoDAOTest {
     @Test
     public void testInsert() throws Exception {
         TodoVO vo = TodoVO.builder().
-                title("Sample Title Todo ...")
+                title("Sample Title Todo ... 3")
                 .dueDate(LocalDate.now())
                 .finished(true)
                 .build();
@@ -40,5 +41,28 @@ public class TodoDAOTest {
     public void testSelectAll() throws Exception {
         List<TodoVO> list = todoDAO.selectAll();
         list.forEach(System.out::println);
+    }
+
+    @Test
+    public void testSelectOne() throws Exception {
+        TodoVO vo = todoDAO.selectOne(3L);
+        System.out.println(vo);
+    }
+
+    @Test
+    public void testUpdate() throws Exception {
+        TodoVO vo = TodoVO.builder()
+                .tno(1L)
+                .title("Update Title")
+                .dueDate(LocalDate.now())
+                .finished(true)
+                .build();
+
+        todoDAO.updateOne(vo);
+    }
+
+    @Test
+    public void testDelete() throws Exception {
+        todoDAO.delete(1L);
     }
 }
